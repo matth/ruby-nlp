5c764d0d592ea3e15ce19d4fc17fa015198c6a07
Matt Haynes <matt@matthaynes.net>
2012-04-16 12:20:31 +0100

    Remove dodgy attempts at stuff

--- lib/nlp.rb
+++ lib/nlp.rb
@@ -2,11 +2,6 @@ $: << File.dirname(__FILE__)
 
 require 'nlp/classifiers/naive_bayes'
 
-require 'nlp/data_structures/trie'
-
-require 'nlp/ngram_index'
-
 require 'nlp/stemmers/porter_stemmer'
 
-require 'nlp/tokenizer'
 require 'nlp/utils'
